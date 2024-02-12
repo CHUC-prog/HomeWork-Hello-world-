dd08a33b3a1112e3b48239d3442eb5085a13ecb5
et1kemz <149301470+et1kemz@users.noreply.github.com>
2024-02-12 22:27:47 +0500

    часть знаний (ништяк крч)

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+    namespace ConsoleApp1
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Игра: 'Русская рулетка'");
+            Console.ReadKey();
+            Random shot = new Random();
+            int a = shot.Next(1,6);
+            for (int i = 1; i < 6; i++) 
+            {
+                if (i == shot.Next (1,6))
+                {
+                    Console.WriteLine("YOU DIED");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Да ты везунчик");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
